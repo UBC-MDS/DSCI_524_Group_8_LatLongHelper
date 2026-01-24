d55f827afb87fe940f997e395682766ba97818fe
ahass9 <ashifa.hassam@gmail.com>
2026-01-23 19:49:51 -0800

    Updated __init__

--- src/latlonghelper/__init__.py
+++ src/latlonghelper/__init__.py
@@ -3,3 +3,15 @@ from importlib.metadata import version
 __version__ = version("latlonghelper")
 
 from .__about__ import __version__
+
+# Import public functions/classes for API reference
+from .lat_long_distance import LatLongDistance
+from .lat_long_binning import LatLongBinning
+from .plot_binned_lat_long import PlotBinnedLatLong
+
+# Optional: define __all__ to list public API
+__all__ = [
+    "LatLongDistance",
+    "LatLongBinning",
+    "PlotBinnedLatLong",
+]
